48b916267f6db1cba3744e17e53bef217aa2c853
Debora Leite <debora14leite@gmail.com>
2021-04-19 20:37:39 -0300

    Add files via upload

---
+++ src/main/java/aco/Ant.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aco;
+
+/**
+ *
+ * @author deb_c
+ */
+public class Ant {
+    
+    double alpha;
+    double beta;
+    
+    public void findSolution(){
+    
+    }
+    
+    public void findNextNode(){
+    
+    }
+    
+}
---
+++ src/main/java/aco/Colony.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aco;
+
+/**
+ *
+ * @author deb_c
+ */
+public class Colony {
+    
+    public void findSolutions(){
+    
+    }
+}
