846cc77cf0d5af397f994488dd43b1c4d32d3d01
Diego Pinheiro <diegopinheiro@Diegos-MacBook-Pro.local>
2021-04-20 02:32:01 -0300

    new structure

--- src/main/java/aco/transitionStrategies/CalculateTransitionStrategy.java
+++
@@ -1,9 +0,0 @@
-package aco.transitionStrategies;
-
-import aco.Ant;
-import aco.graph.Node;
-
-public abstract class CalculateTransitionStrategy {
-	
-	public abstract double probability(Node nodeId, Node nodeOut, Ant ant);
-}
