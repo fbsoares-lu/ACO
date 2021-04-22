37f4bfcc72b1ab5bbbd47a99f75f70bd907d9629
Lucas Soares <lsoareshfb@gmail.com>
2021-04-22 20:29:47 -0300

    Create a new package

--- myaco/src/main/java/com/diegopinheiro/computacaonatural/myaco/pheromoneStrategy/PheromoneStrategy.java
+++ myaco/src/main/java/com/diegopinheiro/computacaonatural/myaco/pheromoneStrategy/PheromoneStrategy.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package pheromoneStrategy;
+package com.diegopinheiro.computacaonatural.myaco.pheromoneStrategy;
 
 import com.diegopinheiro.computacaonatural.myaco.colony.Ant;
 import com.diegopinheiro.computacaonatural.myaco.colony.Colony;
